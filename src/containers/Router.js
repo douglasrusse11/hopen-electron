56f7a1b7990c7a519fff21c57c369595f634f03e
Douglas Russell <douglas.russell7@btinternet.com>
2022-02-02 09:32:11 +0000

    adds Header

--- src/containers/Router.js
+++ src/containers/Router.js
@@ -1,9 +1,34 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {useState, useEffect} from 'react';
+import {Auth, Hub} from 'aws-amplify';
+import Header from '../components/Header';
 
 const Router = () => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        getUser()
+        Hub.listen('auth', (data) => {
+            const { payload: { event }} = data
+            if (event === 'signIn' || event === 'signOut') getUser()
+        })
+    }, [])
+
+    const getUser = async () => {
+        try {
+            const data = await Auth.currentAuthenticatedUser();
+            const userInfo = {username: data.username, isAdmin: data.signInUserSession.idToken.payload['cognito:groups'] && data.signInUserSession.idToken.payload['cognito:groups'].includes('Admin'), ...data.attributes};
+            setUser(userInfo);
+        } catch (err) {
+            setUser(null);
+            console.log('error: ', err);
+        }
+    }
 
     return (
-        <h1>Router goes here</h1>
+        <BrowserRouter >
+            <Header user={user} />
+        </BrowserRouter>
     )
 }
 
