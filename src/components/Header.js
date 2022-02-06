68fcfb2675924a0a022034034b7e07655db9fd88
Douglas Russell <douglas.russell7@btinternet.com>
2022-02-06 16:18:25 +0000

    adds Modal window for login

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,29 @@ import { useState } from 'react';
 import { Auth } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import { Link } from 'react-router-dom';
+import Modal from 'react-modal';
 import '@aws-amplify/ui-react/styles.css';
 
+const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
+Modal.setAppElement('#root');
 const Header = function({user, displayMenu, setDisplayMenu}) {
+    let subtitle;
 
     const [displayLogin, setDisplayLogin] = useState(false);
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        subtitle.style.color = '#f00';
+      }
 
     return (
         <>
@@ -25,7 +43,13 @@ const Header = function({user, displayMenu, setDisplayMenu}) {
                 { user ? <h1 style={styles.heading} onClick={() => {Auth.signOut(); setDisplayLogin(false)}}>Sign Out</h1> : <h1 style={styles.heading} onClick={() => setDisplayLogin(!displayLogin)}>Sign In</h1> }
             </div>
         </div>
-        { displayLogin && <Authenticator /> }
+        <Modal
+            isOpen={displayLogin}
+            onAfterOpen={afterOpenModal}
+            onRequestClose={() => setDisplayLogin(false)}
+            style={customStyles}
+            contentLabel="Example Modal"
+        ><Authenticator/ ></Modal>
         </>
     )
 }
